6346df996b38ff18d782f2063868edc75dfc86f9
Huzam229 <huzamkhan99@gmail.com>
2026-03-10 12:46:12 +0500

    Change the base url in the front-end

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://chatterly-backend-sigma.vercel.app/api",
   withCredentials: true, // send the cookies with the request
 });
 
